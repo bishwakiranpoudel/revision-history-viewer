65876a249be5a6080a704cc59333429bde508ca2
Bishwa <bishwakiran725@gmail.com>
2025-04-15 12:40:49 +0545

    titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <title>Revision checker</title>
+      </head>
       <body>{children}</body>
     </html>
   )
